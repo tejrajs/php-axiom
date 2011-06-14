9d5cf6abef78362b5554ab2b94fec584a36cadf0
TchernoBen@gmail.com <TchernoBen@gmail.com@43f80fda-7e6a-bd16-efa4-a7ce88df5186>
2011-06-14 15:14:57 +0000

    DEV: Adding JSonDataIslandHelper

--- trunk/application/view/summary/test.html.php
+++ trunk/application/view/summary/test.html.php
@@ -11,3 +11,12 @@ $table
     ->addRow(array('test1', 'test2'), 'foot');
 
 echo $table;
+?>
+<div id="dataisland"><?=JSonDataIslandHelper::export(array('test' => array(1,2,3)))?>Data Island</div>
+<script type="text/javascript">
+var init = function () {
+	var json = eval('('+document.getElementById('dataisland').firstChild.data+')');
+	alert(json.test[2]);
+};
+window.onload = init;
+</script>
