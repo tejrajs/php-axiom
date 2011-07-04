d96b37292917f739cf484beda0ccf344e4ccebd9
TchernoBen@gmail.com <TchernoBen@gmail.com@43f80fda-7e6a-bd16-efa4-a7ce88df5186>
2011-07-04 17:44:27 +0000

    DEV: Removing admin capability and relocating them in separed module

--- trunk/application/config/bootstrap/routes.php
+++ trunk/application/config/bootstrap/routes.php
@@ -14,5 +14,6 @@ require_once LIBRARY_PATH . '/Router.class.php';
 
 Router::setConfig();
 Router::addRoute('default', array('index'));
-Router::addRoute('`^((?<lang>[[:alnum:]]{2})/)?admin/(?<admrt>[[:alnum:]]{3,})/?(?<admact>[[:alnum:]]{3,})?/?$`', array('admin','module'));
+//Router::addRoute('`^((?<lang>[[:alnum:]]{2})/)?admin/(?<admrt>[[:alnum:]]{3,})/?(?<admact>[[:alnum:]]{3,})?/?$`', array('admin','module'));
+Router::addRoute('~^((?<lang>[[:alnum:]]{2})/)?(login|admin)$~', array('admin', 'index'));
 
