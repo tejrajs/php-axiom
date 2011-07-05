4094efb2781b48c8ee416debcea26cedddd96af3
TchernoBen@gmail.com <TchernoBen@gmail.com@43f80fda-7e6a-bd16-efa4-a7ce88df5186>
2011-07-05 11:47:25 +0000

    DEV: Removing admin capability and relocating them in separed module

--- trunk/application/view/admin/file.json.php
+++
@@ -1,8 +0,0 @@
-<?=json_encode(array(
-    i18n('admin.files.file.name') => $file->filename,
-    i18n('admin.files.file.size') => i18n('units.kB', ceil($file->getSize() / 1024)),
-    i18n('admin.files.file.path') => src("upload/$file->filename"),
-    i18n('admin.files.file.type') => $file->mime_type,
-    i18n('admin.files.file.date') => $file->upload,
-    i18n('admin.files.file.user') => $file->getUser()->login,
-))?>
