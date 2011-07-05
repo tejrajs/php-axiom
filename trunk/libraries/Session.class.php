f1008cbee24692bd29c75e3efd5283f9991b64a2
TchernoBen@gmail.com <TchernoBen@gmail.com@43f80fda-7e6a-bd16-efa4-a7ce88df5186>
2011-07-05 16:26:57 +0000

    DEV: Removing admin capability and relocating them in separed module

--- trunk/libraries/Session.class.php
+++ trunk/libraries/Session.class.php
@@ -2,13 +2,26 @@
 
 class Session {
     
+    protected static $_config;
+    
     protected $_session_parameters;
+    
+    public static function setConfig (array $config = array()) {
+        $default = array(
+            'index' => null,
+        );
+        
+        self::$_config = $config + $default;
+    }
     
     public function __construct () {
         if (!session_id())
             self::start();
         
-        $this->_session_parameters = & $_SESSION;
+        if (self::$_config['index'])
+            $this->_session_parameters = & $_SESSION[self::$_config['index']];
+        else
+            $this->_session_parameters = & $_SESSION;
     }
     
     public function __get ($key) {
