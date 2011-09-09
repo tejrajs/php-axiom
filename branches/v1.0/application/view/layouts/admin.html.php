f0dd22f80a8e786b505d601db0f55c4ed0b71199
TchernoBen@gmail.com <TchernoBen@gmail.com@43f80fda-7e6a-bd16-efa4-a7ce88df5186>
2011-09-09 11:56:53 +0000

    DEV: Backend Theme

--- branches/v1.0/application/view/layouts/admin.html.php
+++ branches/v1.0/application/view/layouts/admin.html.php
@@ -4,11 +4,12 @@
 		<title><?=i18n('admin.title')?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="Content-Language" content="<?=$lang?>" />
-        <link href="<?=src('css/smoothness/jquery-ui-1.8.12.custom.css')?>" type="text/css" rel="stylesheet" media="screen" />
+        <link href="<?=src('css/lightness/jquery-ui-1.8.16.custom.css')?>" type="text/css" rel="stylesheet" media="screen" />
         <link href="<?=src('css/admin.css')?>" type="text/css" rel="stylesheet" media="screen" />
         <link href="<?=src('css/tipTip.css')?>" type="text/css" rel="stylesheet" media="screen" />
         <link href="<?=src('css/cleditor/cleditor.css')?>" type="text/css" rel="stylesheet" media="screen" />
         <link href="<?=src('css/fancybox/jquery.fancybox-1.3.4.css')?>" type="text/css" rel="stylesheet" media="screen" />
+		<link href="<?=src('css/tablesorter/tablesorter.css')?>" type="text/css" rel="stylesheet" media="screen" />
         <link href="<?=src('img/axiom_16.png')?>" type="image/png" rel="icon" media="screen" />
         <script src="<?=src('js/jquery-1.5.1.min.js')?>" type="text/javascript"></script>
         <script src="<?=src('js/jquery.tablesorter.min.js')?>" type="text/javascript"></script>
@@ -18,7 +19,6 @@
         <script src="<?=src('js/jquery.cleditor.min.js')?>" type="text/javascript"></script>
         <script src="<?=src('js/jquery.fancybox-1.3.4.js')?>" type="text/javascript"></script>
 		<script type="text/javascript">
-		<!--
 		$(function () {
 			$('.warning,.error').click(function () { $(this).hide('blind'); });
 			$('.tiptip').tipTip({defaultPosition: 'top'});
@@ -54,13 +54,22 @@
 				return confirm('<?=i18n('admin.confirm_action')?>');
 			});
 
-			$("tbody > tr").hover(
-				function () { $(this).addClass('ui-state-active', 500); },
-				function () { $(this).removeClass('ui-state-active', 500); }
+			$("tbody tr").hover(
+				function () { $(this).addClass('ui-state-active'); },
+				function () { $(this).removeClass('ui-state-active'); }
 			);
 		});
-		//-->
 		</script>
+		<!--  JQUERY UI THEME SWITCHER -->
+		<!--
+		<link type="text/css" rel="stylesheet" href="http://jqueryui.com/themes/base/ui.all.css" />
+        <script type="text/javascript" src="http://jqueryui.com/themeroller/themeswitchertool/"></script>
+        <script type="text/javascript">
+        $(function () {
+        	$('#switcher').themeswitcher();
+        });
+        </script>
+		-->
 	</head>
 	<body>
 		<div id="wrapper">
